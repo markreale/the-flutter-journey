6cdb608562c6fca77ddaeae0b3ebfbf7b7ae4ead
Mark Reale <reale.mark@gmail.com>
2018-10-31 20:53:15 -0400

    Build your own scrolling
    Who would have thought that this is something that you need to do yourself?

---
+++ scrolling/README.md
@@ -0,0 +1,8 @@
+# scrolling
+
+In this world, you need to program your own scrolling.
+
+## Getting Started
+
+For help getting started with Flutter, view our online
+[documentation](https://flutter.io/).
---
+++ scrolling/lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(MaterialApp(
+    home: new SingleChildScrollView(
+      child: new Text('This is an important message. This is an important message. '
+          'You are receiving the message due to its importance. It took a lot of'
+          'work to get this message here. We thought that if we sent a long'
+          'message, and the message was too long to fit on your entire screen,'
+          'your device would be intelligent enough to make the screen scroll in'
+          'order to read the whole message. But no. This is not the default '
+          'behaviour in a Flutter application. Rumor is this is not default '
+          'behaviour in other mobile interface systems as well. How do people '
+          'live like this? These are the types of things developers need to deal'
+          'with. No default scrolling. What a life.'),
+  )));
+}
